63f761a94cc59227be9b3d086f9a08225f1045e6
ConnorKunstek <ConnorKunstek@ConnorsMacBookPro.local>
2017-03-28 21:24:10 -0400

    Version 3
    March 28, 9:25pm

--- travelingSalesman/City.cpp
+++ travelingSalesman/City.cpp
@@ -4,15 +4,14 @@
 //Traveling Salesman
 
 #include<iostream>
-#include<vector>
+
 #include "City.h"
-#include <map>
 
 using namespace std;
 
 City::City()
 {
-  name = NULL;
+  name = "NULL";
   latitude = 0.0;
   longitude = 0.0;
 };
@@ -48,5 +47,5 @@ void City::display1()
 
 void City::display2()
 {
-  cout << "\t Name: " << name << "\t Latitude: " << latitude << " \t Longitude: " << longitude;
+  cout << endl << "\t Name: " << name << "\t Latitude: " << latitude << " \t Longitude: " << longitude;
 }
